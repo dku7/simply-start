c82305d3b3e384c0ef78ad20ce4e6ba281bdb955
David Ku <dku7@proton.me>
2024-12-12 07:01:28 +0000

    use icons on buttons

--- src/components/IntervalSettingButton.tsx
+++ src/components/IntervalSettingButton.tsx
@@ -1,14 +1,20 @@
+import { CiSquarePlus, CiSquareMinus } from "react-icons/ci";
+
 interface IntervalSettingButtonProps {
   type: string;
   handleClick: () => void;
 }
+
 export default function IntervalSettingButton({
   type,
   handleClick,
 }: IntervalSettingButtonProps) {
-  return (
-    <button className="mx-2 h-4 w-4 rounded border" onClick={handleClick}>
-      {type === "add" ? "+" : "-"}
-    </button>
-  );
+  const buttonType =
+    type === "add" ? (
+      <CiSquarePlus size={32} />
+    ) : type === "minus" ? (
+      <CiSquareMinus size={32} />
+    ) : null;
+
+  return <button onClick={handleClick}>{buttonType}</button>;
 }
