9e69d7b081594733f3cc88d28a99635af888a629
David Ku <dku7@proton.me>
2024-12-12 16:07:29 +0000

    make button title dynamic depending on current status

--- src/components/TimerButton.tsx
+++ src/components/TimerButton.tsx
@@ -1,17 +1,24 @@
 import { memo } from "react";
+import { TimerStatusType } from "../types/types";
 
 interface TimerButtonProps {
-  title: string;
-  handleCountdown: () => void;
+  status: TimerStatusType;
+  handleClick: () => void;
 }
 
 export const TimerButton = memo(function TimerButton({
-  title,
-  handleCountdown,
+  status,
+  handleClick,
 }: TimerButtonProps) {
+  const buttonCaptions: Record<TimerStatusType, string> = {
+    "Not Started": "Start",
+    Started: "Pause",
+    Paused: "Resume",
+  };
+
   return (
-    <button onClick={handleCountdown} className="mr-2 rounded border p-1">
-      {title}
+    <button onClick={handleClick} className="mr-2 w-40 rounded border p-1">
+      {buttonCaptions[status]}
     </button>
   );
 });
