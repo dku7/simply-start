2e61280bc08389e416a0e38b4c66034c7af1cd0c
David Ku <dku7@proton.me>
2024-12-12 11:12:56 +0000

    refactor getIntervalSeconds

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -2,9 +2,7 @@ import { IntervalType } from "../types/types";
 import { defaultIntervals } from "../constants/constants";
 
 export function getIntervalSeconds(type: IntervalType): number {
-  const seconds = Number(localStorage.getItem(type));
-
-  return seconds ? seconds : defaultIntervals[type];
+  return Number(localStorage.getItem(type) ?? defaultIntervals[type]);
 }
 
 export function saveIntervalSeconds(type: IntervalType, seconds: number) {
