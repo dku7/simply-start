063c5587170aabf9ed80065a9f7cbd1f7331d002
David Ku <dku7@proton.me>
2024-12-13 08:42:14 +0000

    change heading capitalisation

--- src/components/Heading.tsx
+++ src/components/Heading.tsx
@@ -1,7 +1,7 @@
 export default function Heading() {
   return (
     <header>
-      <h1 className="text-3xl font-extrabold">pomodomoro</h1>
+      <h1 className="text-3xl font-extrabold">Pomodomoro</h1>
     </header>
   );
 }
