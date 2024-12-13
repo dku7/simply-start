d21517a00293a76126cbd75cf55dd50c19de186b
David Ku <dku7@proton.me>
2024-12-13 09:43:26 +0000

    add functions to save and get completed sessions from localStorage

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -8,3 +8,11 @@ export function getIntervalSeconds(type: IntervalType): number {
 export function saveIntervalSeconds(type: IntervalType, seconds: number) {
   localStorage.setItem(type, seconds.toString());
 }
+
+export function getCompletedSessions(): number {
+  return Number(localStorage.getItem("Sessions") ?? 0);
+}
+
+export function saveCompletedSessions(sessions: number) {
+  localStorage.setItem("Sessions", sessions.toString());
+}
