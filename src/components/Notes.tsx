7eb92079c571787ca1f78f85575019810b898e52
David Ku <dku7@proton.me>
2024-12-16 08:43:27 +0000

    create Notes component

---
+++ src/components/Notes.tsx
@@ -0,0 +1,11 @@
+export default function Notes() {
+  return (
+    <>
+      <textarea
+        name="notes"
+        id="notes"
+        className="h-96 w-full border border-slate-300 bg-slate-200 p-2 px-2 font-mono text-slate-600 focus:border-slate-400 focus:outline-none focus:ring-0"
+      ></textarea>
+    </>
+  );
+}
